798d3b0d2906442e2396737cff62c47297a11611
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-01-10 18:38:51 +0400

    initial project setup

---
+++ KlipyiOS-demo/KlipyiOS_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KlipyiOS_demoApp.swift
+//  KlipyiOS-demo
+//
+//  Created by Tornike Gomareli on 10.01.25.
+//
+
+import SwiftUI
+
+@main
+struct KlipyiOS_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
