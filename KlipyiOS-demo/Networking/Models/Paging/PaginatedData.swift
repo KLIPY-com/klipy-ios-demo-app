39d11b9ee553c9b9e53639df78ff9b2d92d4a40e
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-01-13 17:47:31 +0400

    paginated generic

--- KlipyiOS-demo/Networking/Models/Paging/PaginatedData.swift
+++ KlipyiOS-demo/Networking/Models/Paging/PaginatedData.swift
@@ -1,13 +1,22 @@
-struct PaginatedData: Codable {
-    let data: [ClipItem]
-    let currentPage: Int
-    let perPage: Int
-    let hasNext: Bool
-    
-    enum CodingKeys: String, CodingKey {
-        case data
-        case currentPage = "current_page"
-        case perPage = "per_page"
-        case hasNext = "has_next"
-    }
+//
+//  PaginatedData.swift
+//  KlipyiOS-demo
+//
+//  Created by Tornike Gomareli on 13.01.25.
+//
+
+import Foundation
+
+struct PaginatedData<T: Codable>: Codable {
+  let data: [T]
+  let currentPage: Int
+  let perPage: Int
+  let hasNext: Bool
+  
+  enum CodingKeys: String, CodingKey {
+    case data
+    case currentPage = "current_page"
+    case perPage = "per_page"
+    case hasNext = "has_next"
+  }
 }
