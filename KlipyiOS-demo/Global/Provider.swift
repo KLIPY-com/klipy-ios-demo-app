3a07d3ef2789f393f58b2576af02e83303b43349
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-01-14 15:36:31 +0400

    wip

--- KlipyiOS-demo/Global/Provider.swift
+++ KlipyiOS-demo/Global/Provider.swift
@@ -15,7 +15,7 @@ extension NetworkingProvider {
 
 extension RestApi {
   public static var liveValue: RestApi {
-    // TODO: We need to crypt it
+    // TODO: We need to crypt it and hide from public git
     let api_key = "685pfsUU3EODe5rjG3li8rLUdfyydxxfh8fPym7wM5dvr0jklulSi6g5BSWlL3zG"
     return RestApi(baseURL: URL(string: "https://api.klipy.co/api/v1/\(api_key)")!, provider: NetworkingProvider.liveValue)
   }
