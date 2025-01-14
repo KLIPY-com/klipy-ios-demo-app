cd3e2c81eba8eefe89c591f071d3a53d7bd78493
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-01-14 15:36:28 +0400

    wip

---
+++ KlipyiOS-demo/App/ChatFeature/ChatFeatureViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  ChatFeatureViewModel.swift
+//  KlipyiOS-demo
+//
+//  Created by Tornike Gomareli on 14.01.25.
+//
+
+import Observation
+import SwiftUI
+import Foundation
+
+@Observable
+final class ChatFeatureViewModel {
+  
+}
