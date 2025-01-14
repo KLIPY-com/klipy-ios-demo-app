a1b607aa286b9618abe2f9a53cad5aff26f42e19
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-01-14 15:05:19 +0400

    chat preview

--- KlipyiOS-demo/ContentView.swift
+++ KlipyiOS-demo/ContentView.swift
@@ -8,17 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+  var body: some View {
+    NavigationStack {
+      VStack(spacing: 0) {
+        NavigationLink(destination: ChatView()) {
+          ChatPreview(
+            name: "John",
+            lastMessage: "Seen",
+            time: "19.02.14",
+            unreadCount: 2
+          )
         }
-        .padding()
+        .buttonStyle(PlainButtonStyle())
+        .padding(.top, 1)
+        
+        Spacer()
+      }
+      .navigationBarTitleDisplayMode(.inline)
+      .toolbar {
+        // Left Menu Button
+        ToolbarItem(placement: .topBarLeading) {
+          Button(action: {
+            print("Search")
+          }) {
+            Image(systemName: "line.3.horizontal")
+              .foregroundColor(Color(hex: "1E68D7"))
+          }
+        }
+
+        ToolbarItem(placement: .topBarTrailing) {
+          Button(action: {
+            print("Search")
+          }) {
+            Image(systemName: "magnifyingglass")
+              .foregroundColor(Color(hex: "1E68D7"))
+          }
+        }
+      }
     }
+  }
 }
 
+// Preview
 #Preview {
-    ContentView()
+  ContentView()
 }
