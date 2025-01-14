b6ac7bbb6a2da9c84498f323f6fafc65e9eb33df
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-01-14 15:05:08 +0400

    sound manager

--- KlipyiOS-demo/App/Utils/SoundManager.swift
+++ KlipyiOS-demo/App/Utils/SoundManager.swift
@@ -1,17 +1,29 @@
+//
+//  SoundManager.swift
+//  KlipyiOS-demo
+//
+//  Created by Tornike Gomareli on 14.01.25.
+//
+
+import AVFoundation
+import Foundation
+
 // MARK: - Sound Manager
 class SoundManager {
-    static let shared = SoundManager()
-    private var audioPlayer: AVAudioPlayer?
-    
-    func playMessageSound() {
-        guard let soundURL = Bundle.main.url(forResource: "message_sent", withExtension: "wav") else { return }
-        
-        do {
-            audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
-            audioPlayer?.prepareToPlay()
-            audioPlayer?.play()
-        } catch {
-            print("Error playing sound: \(error)")
-        }
+  static let shared = SoundManager()
+  private var audioPlayer: AVAudioPlayer?
+  
+  func playMessageSound() {
+    guard let soundURL = Bundle.main.url(forResource: "message_sent", withExtension: "wav") else {
+      return
     }
+    
+    do {
+      audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
+      audioPlayer?.prepareToPlay()
+      audioPlayer?.play()
+    } catch {
+      print("Error playing sound: \(error)")
+    }
+  }
 }
