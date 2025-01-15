e04eaf16b4104ab3009f8a3728520d26429339c9
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-01-16 01:03:03 +0400

    row layout

--- KlipyiOS-demo/App/MasonryLayout/RowLayout.swift
+++ KlipyiOS-demo/App/MasonryLayout/RowLayout.swift
@@ -1,4 +1,13 @@
+//
+//  RowLayout.swift
+//  KlipyiOS-demo
+//
+//  Created by Tornike Gomareli on 15.01.25.
+//
+
+import Foundation
+
 struct RowLayout {
-    var items: [GridItemLayout]
-    var height: CGFloat
+  var items: [GridItemLayout]
+  var height: CGFloat
 }
