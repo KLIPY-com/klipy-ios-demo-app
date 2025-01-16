4b250a697c5dadbbb15f7d152d64e75e86c39834
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-01-17 02:24:36 +0400

    wip

--- KlipyiOS-demo/Infrastructure/Services/MediaServiceUseCase.swift
+++
@@ -1,5 +0,0 @@
-protocol MediaServiceUseCase {
-    associatedtype Item
-    func fetchTrending(page: Int, perPage: Int) async throws -> AnyResponse<Item>
-    func searchItems(query: String, page: Int, perPage: Int) async throws -> AnyResponse<Item>
-}
