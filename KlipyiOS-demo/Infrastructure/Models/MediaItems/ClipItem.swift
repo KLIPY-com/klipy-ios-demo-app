c04863349af4faa0675b06a3c05af196a2ce8c6d
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-01-17 02:24:14 +0400

    fixed some kinda JSON properties

--- KlipyiOS-demo/Infrastructure/Models/MediaItems/ClipItem.swift
+++ KlipyiOS-demo/Infrastructure/Models/MediaItems/ClipItem.swift
@@ -7,13 +7,18 @@
 
 import Foundation
 
-struct ClipItem: Codable {
+struct ClipItem: MediaItem {
+  static func == (lhs: ClipItem, rhs: ClipItem) -> Bool {
+    return lhs.id == rhs.id
+  }
+  
+  let id: Int
   let url: String
   let title: String
   let slug: String
   let blurPreview: String
-  let file: FileFormats
-  let fileMeta: FileMeta
+  let file: ClipFile
+  let fileMeta: ClipFileMeta
   let type: MediaType
   
   enum CodingKeys: String, CodingKey {
@@ -25,4 +30,36 @@ struct ClipItem: Codable {
     case fileMeta = "file_meta"
     case type
   }
+  
+  // Custom init from decoder that sets the ID
+  init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    
+    self.id = Int.random(in: 1...Int.max)
+    
+    self.url = try container.decode(String.self, forKey: .url)
+    self.title = try container.decode(String.self, forKey: .title)
+    self.slug = try container.decode(String.self, forKey: .slug)
+    self.blurPreview = try container.decode(String.self, forKey: .blurPreview)
+    self.file = try container.decode(ClipFile.self, forKey: .file)
+    self.fileMeta = try container.decode(ClipFileMeta.self, forKey: .fileMeta)
+    self.type = try container.decode(MediaType.self, forKey: .type)
+  }
+}
+
+struct ClipFile: Codable {
+  var mp4: String
+  var gif: String
+  var webp: String
+}
+
+struct ClipFileMeta: Codable {
+  var mp4: ClipMeta
+  var gif: ClipMeta
+  var webp: ClipMeta
+}
+
+struct ClipMeta: Codable {
+  var width: Int
+  var height: Int
 }
