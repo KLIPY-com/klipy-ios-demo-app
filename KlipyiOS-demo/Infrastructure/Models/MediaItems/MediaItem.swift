cd7586df2deaae933f0bfc294f3125023c470773
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-01-17 02:24:24 +0400

    protocol for items

--- KlipyiOS-demo/Infrastructure/Models/MediaItems/MediaItem.swift
+++ KlipyiOS-demo/Infrastructure/Models/MediaItems/MediaItem.swift
@@ -1,4 +1,16 @@
-protocol MediaItem {
-    var id: Int64 { get }
-    // Add any other common properties needed for layout
+//
+//  MediaItem.swift
+//  KlipyiOS-demo
+//
+//  Created by Tornike Gomareli on 17.01.25.
+//
+
+import Foundation
+
+protocol MediaItem: Codable, Equatable {
+  var id: Int { get }
+  var title: String { get }
+  var slug: String { get }
+  var blurPreview: String { get }
+  var type: MediaType { get }
 }
