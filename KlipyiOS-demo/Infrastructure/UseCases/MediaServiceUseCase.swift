b7a010f8d2c44460511357775b77958f99e776ee
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-01-17 02:24:43 +0400

    base use case

--- KlipyiOS-demo/Infrastructure/UseCases/MediaServiceUseCase.swift
+++ KlipyiOS-demo/Infrastructure/UseCases/MediaServiceUseCase.swift
@@ -8,7 +8,8 @@
 import Foundation
 
 protocol MediaServiceUseCase {
-  associatedtype Item
-  func fetchTrending(page: Int, perPage: Int) async throws -> AnyResponse<Item>
+  associatedtype Item: Codable
+
+  func fetchTrendingItems(page: Int, perPage: Int) async throws -> AnyResponse<Item>
   func searchItems(query: String, page: Int, perPage: Int) async throws -> AnyResponse<Item>
 }
