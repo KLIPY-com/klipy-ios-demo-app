5a6220d665e69285a183f0add8f11bfb6bd46993
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-01-21 01:56:55 +0400

    indent

--- KlipyiOS-demo/KlipyiOS_demoApp.swift
+++ KlipyiOS-demo/KlipyiOS_demoApp.swift
@@ -9,17 +9,17 @@ import SwiftUI
 
 @main
 struct KlipyiOS_demoApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-            .onAppear {
-              let useCase = HealthCheckServiceUseCase()
-              let gifsUseCase = GifServiceUseCase()
-              Task {
-                try await useCase.fetchUpdateInfo()
-                try await gifsUseCase.fetchTrending(page: 1, perPage: 10)
-              }
-            }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+        .onAppear {
+          let useCase = HealthCheckServiceUseCase()
+          let gifsUseCase = GifServiceUseCase()
+          Task {
+            try await useCase.fetchUpdateInfo()
+            try await gifsUseCase.fetchTrending(page: 1, perPage: 10)
+          }
         }
     }
+  }
 }
