ffaf2fae67a63892c69de1aa6e63ec0de7dbb602
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-01-28 02:10:43 +0400

    onClick implemented in onRowPressed

--- KlipyiOS-demo/App/MasonryLayout/RowView.swift
+++ KlipyiOS-demo/App/MasonryLayout/RowView.swift
@@ -13,18 +13,25 @@ struct RowView: View {
   let isLastRow: Bool
   let onLoadMore: () -> Void
   let previewModel: PreviewViewModel
+  let onRowPressed: (GridItemLayout) -> Void
   
   var body: some View {
     ZStack(alignment: .topLeading) {
       ForEach(row.items) { item in
-        LazyGIFView(item: item, previewModel: previewModel)
-          .frame(width: item.width, height: item.height)
-          .offset(x: item.xPosition, y: 0)
-          .onAppear {
-            if isLastRow && item.id == row.items.last?.id {
-              onLoadMore()
-            }
+        LazyGIFView(
+          item: item,
+          previewModel: previewModel,
+          onClick: {
+            onRowPressed(item)
           }
+        )
+        .frame(width: item.width, height: item.height)
+        .offset(x: item.xPosition, y: 0)
+        .onAppear {
+          if isLastRow && item.id == row.items.last?.id {
+            onLoadMore()
+          }
+        }
       }
     }
     .frame(maxWidth: .infinity, maxHeight: row.height, alignment: .leading)
