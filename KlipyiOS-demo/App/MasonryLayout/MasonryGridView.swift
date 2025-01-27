876a7f34047bf51400c1eaca5c9f134b6ac69263
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-01-28 02:10:20 +0400

    formatting

--- KlipyiOS-demo/App/MasonryLayout/MasonryGridView.swift
+++ KlipyiOS-demo/App/MasonryLayout/MasonryGridView.swift
@@ -6,22 +6,27 @@
 //
 
 import SwiftUI
-import Pow
 
 struct MasonryGridView: View {
   let rows: [RowLayout]
   let onLoadMore: () -> Void
-
+  
   let onSend: (GridItemLayout) -> Void
   let onReport: (String, ReportReason) -> Void
   
   @StateObject private var previewModel = PreviewViewModel()
-
+  
   var body: some View {
     ScrollView(showsIndicators: false) {
       LazyVStack(spacing: 0) {
         ForEach(rows.indices, id: \.self) { rowIndex in
-          RowView(row: rows[rowIndex], isLastRow: rowIndex == rows.count - 1, onLoadMore: onLoadMore, previewModel: previewModel)
+          RowView(
+            row: rows[rowIndex],
+            isLastRow: rowIndex == rows.count - 1,
+            onLoadMore: onLoadMore,
+            previewModel: previewModel) { pressedItem in
+              onSend(pressedItem)
+            }
         }
       }
     }
@@ -36,13 +41,13 @@ struct MasonryGridView: View {
         onReport: { url, reportReason in
           onReport(url, reportReason)
         }) {
-        withAnimation(.spring(response: 0.3, dampingFraction: 0.8)) {
-          previewModel.selectedItem = nil
-          previewModel.isDragging = false
-          previewModel.dragOffset = .zero
-          previewModel.dragScale = 1
+          withAnimation(.spring(response: 0.3, dampingFraction: 0.8)) {
+            previewModel.selectedItem = nil
+            previewModel.isDragging = false
+            previewModel.dragOffset = .zero
+            previewModel.dragScale = 1
+          }
         }
-      }
     }
   }
 }
