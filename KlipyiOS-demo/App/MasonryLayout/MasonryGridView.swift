8f102b652061fd395e0f35582ebe890fcf0f6ef0
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-02-11 01:56:14 +0400

    correct sizing

--- KlipyiOS-demo/App/MasonryLayout/MasonryGridView.swift
+++ KlipyiOS-demo/App/MasonryLayout/MasonryGridView.swift
@@ -22,15 +22,17 @@ struct MasonryGridView: View {
     ScrollView(showsIndicators: false) {
       LazyVStack(spacing: 0) {
         ForEach(rows.indices, id: \.self) { rowIndex in
-          RowView(
-            row: rows[rowIndex],
-            isLastRow: rowIndex == rows.count - 1,
-            onLoadMore: onLoadMore,
-            previewModel: previewModel,
-            isFocused: _isFocused) { pressedItem in
-              onSend(pressedItem)
-            }
-            .padding(.bottom, 3)
+            RowView(
+              row: rows[rowIndex],
+              isLastRow: rowIndex == rows.count - 1,
+              onLoadMore: onLoadMore,
+              previewModel: previewModel,
+              isFocused: _isFocused) { pressedItem in
+                onSend(pressedItem)
+              }
+              .padding(.bottom, 3)
+              /// If !hasNext == true || rowIndex != rows.count - 1 ? 1 : 0
+              .opacity(rowIndex != rows.count - 1 ? 1 : 0)
         }
       }
     }
