726a96669bfb0d4c3187cc8b4b15c3feb8581b58
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-02-10 05:14:43 +0400

    fixed bug for recents

--- KlipyiOS-demo/App/DynamicMediaView/DynamicMediaView.swift
+++ KlipyiOS-demo/App/DynamicMediaView/DynamicMediaView.swift
@@ -1,5 +1,5 @@
 //
-//  GifGridView.swift
+//  DynamicMediaView.swift
 //  KlipyiOS-demo
 //
 //  Created by Tornike Gomareli on 15.01.25.
@@ -9,26 +9,25 @@ import SwiftUI
 import AlertToast
 
 actor SearchDebouncer {
-    private var task: Task<Void, Never>?
-    
-    func debounce(
-        for duration: Duration = .milliseconds(300),
-        action: @escaping () async -> Void
-    ) {
-        task?.cancel()
-        task = Task {
-            try? await Task.sleep(for: duration)
-            guard !Task.isCancelled else { return }
-            await action()
-        }
+  private var task: Task<Void, Never>?
+  
+  func debounce(
+    for duration: Duration = .milliseconds(300),
+    action: @escaping () async -> Void
+  ) {
+    task?.cancel()
+    task = Task {
+      try? await Task.sleep(for: duration)
+      guard !Task.isCancelled else { return }
+      await action()
     }
+  }
 }
 
 struct DynamicMediaView: View {
   @Bindable private var viewModel = DynamicMediaViewModel()
   
   @State private var searchText = ""
-  @State private var categorySearchText = ""
   @State private var selectedCategory: MediaCategory?
   @State private var rows: [RowLayout] = []
   
@@ -40,7 +39,7 @@ struct DynamicMediaView: View {
   
   @Environment(\.dismiss) private var dismiss
   var searchDebouncer = SearchDebouncer()
-
+  
   private let calculator = MasonryLayoutCalculator()
   
   var body: some View {
@@ -48,50 +47,42 @@ struct DynamicMediaView: View {
       VStack(spacing: 0) {
         MediaSearchBar(
           searchText: $searchText,
-          selectedCategory: $selectedCategory,
+          selectedCategory: $viewModel.activeCategory,
           isFocused: _isSearchFocused,
           categories: viewModel.categories
         )
-        .onChange(of: selectedCategory) {_, newCategory in
-          if let categoryName = newCategory {
-            switch categoryName.type {
-            case .trending:
-              Task {
-                let items = try await viewModel.baseLoadTrendingItems()
-                await MainActor.run {
-                  viewModel.items = items
-                }
-              }
-            case .recents:
-              Task {
-                let items = try await viewModel.baseLoadRecentItems()
-                await MainActor.run {
-                  viewModel.items = items
-                }
-              }
-            case .none:
-              categorySearchText = categoryName.name
+        .onChange(of: viewModel.activeCategory) { _, newCategory in
+          guard let category = newCategory else {
+            return
+          }
+  
+          viewModel.resetState()
+          switch category.type {
+          case .trending:
+            Task {
+              let items = try await viewModel.baseLoadTrendingItems()
+              viewModel.items = items
             }
-          } else {
+          case .recents:
             Task {
               let items = try await viewModel.baseLoadRecentItems()
-              await MainActor.run {
-                viewModel.items = items
-              }
+              viewModel.items = items
             }
+          case .none:
+            viewModel.categorySearchText = category.name
           }
         }
         .padding(.bottom, 12)
         .padding(.horizontal, 12)
         .padding(.top, 18)
-        .background(Color.init(hex: "#36383F"))
+        .background(Color(hex: "#36383F"))
         
         mediaContent
         
         mediaTypeSelector
       }
       .toast(isPresenting: $showToast, duration: 5.0) {
-        return AlertToast(
+        AlertToast(
           displayMode: .banner(.pop),
           type: .regular,
           title: "🚓 Klipy moderators will review your report. \nThank you!"
@@ -105,40 +96,34 @@ struct DynamicMediaView: View {
       .navigationBarTitleDisplayMode(.inline)
     }
     .task {
-      await viewModel.loadRecentItems()
       await viewModel.fetchCategories()
+      await viewModel.initialLoad()
     }
     .onChange(of: searchText) { _, newValue in
       Task {
         await searchDebouncer.debounce {
           if newValue.isEmpty {
-            if let items = try? await viewModel.baseLoadRecentItems() {
-              await MainActor.run {
-                viewModel.items = items
-              }
-            }
+            await viewModel.initialLoad()
           } else {
             await viewModel.searchItems(query: newValue)
           }
         }
       }
     }
-    .onChange(of: categorySearchText) { _, newValue in
+    .onChange(of: viewModel.categorySearchText) { _, newValue in
       Task {
         if newValue.isEmpty {
-          let items = try await viewModel.baseLoadRecentItems()
-          await MainActor.run {
-            viewModel.items = items
-          }
+          return
+        } else {
+          await viewModel.searchItems(query: newValue)
         }
-        await viewModel.searchItems(query: newValue)
       }
     }
   }
   
   private var mediaContent: some View {
     ZStack {
-      Color.init(hex: "#36383F")
+      Color(hex: "#36383F")
       MasonryGridView(
         rows: rows,
         onLoadMore: {
@@ -146,14 +131,18 @@ struct DynamicMediaView: View {
             await viewModel.loadNextPageIfNeeded()
           }
         },
+        previewLoaded: { model in
+          Task {
+            guard let mediaItem = viewModel.getMediaItem(by: model.id) else { return }
+            try await viewModel.trackView(for: mediaItem)
+          }
+        },
         onSend: { mediaItem in
           onSend(mediaItem)
-          guard let mediaItem = viewModel.getMediaItem(by: mediaItem.id) else {
-            return
-          }
-          
-          Task {
-            try await viewModel.trackShare(for: mediaItem)
+          if let item = viewModel.getMediaItem(by: mediaItem.id) {
+            Task {
+              try await viewModel.trackShare(for: item)
+            }
           }
         },
         onReport: { url, reason in
@@ -164,46 +153,14 @@ struct DynamicMediaView: View {
       .padding(.horizontal, 10)
       .frame(maxWidth: .infinity)
     }
-    .onChange(of: viewModel.items) { _, _ in
-      rows = calculator.createRows(from: viewModel.items)
+    .onChange(of: viewModel.items) { newValue in
+      rows = calculator.createRows(from: newValue)
     }
-  }
-  
-  private var searchBar: some View {
-    HStack(spacing: 8) {
-      HStack {
-        Image(systemName: "magnifyingglass")
-          .foregroundColor(.gray)
-        TextField("Search \(viewModel.currentType.displayName)", text: $searchText)
-          .font(.system(size: 16))
-        
-        if !searchText.isEmpty {
-          Button(action: {
-            searchText = ""
-            Task {
-              await MainActor.run {
-                viewModel.items = []
-              }
-              //await viewModel.loadRecentItems()
-            }
-          }) {
-            Image(systemName: "xmark.circle.fill")
-              .foregroundColor(.gray)
-          }
-        }
-      }
-      .padding(8)
-      .background(Color(.systemGray6))
-      .cornerRadius(8)
-    }
-    .padding(.horizontal)
-    .padding(.vertical, 8)
-    .background(Color.white)
   }
   
   private var mediaTypeSelector: some View {
     ZStack {
-      Color.init(hex: "#36383F")
+      Color(hex: "#36383F")
         .ignoresSafeArea()
       
       HStack(spacing: 20) {
@@ -226,20 +183,19 @@ struct DynamicMediaView: View {
         viewModel.switchToType(type)
         searchText = ""
         Task {
-          await viewModel.loadRecentItems()
+          await viewModel.initialLoad()
         }
       }
     }) {
       Text(title)
         .font(.system(size: 17, weight: .bold))
-        .foregroundColor(viewModel.currentType == type ? .black : Color.white)
+        .foregroundColor(viewModel.currentType == type ? .black : .white)
         .padding(.vertical, 12)
         .padding(.horizontal, 16)
         .background(
           viewModel.currentType == type ?
-          Rectangle()
-            .fill(Color.init(hex: "F8DC3B"))
-            .cornerRadius(8)
+          RoundedRectangle(cornerRadius: 8)
+            .fill(Color(hex: "F8DC3B"))
           : nil
         )
     }
