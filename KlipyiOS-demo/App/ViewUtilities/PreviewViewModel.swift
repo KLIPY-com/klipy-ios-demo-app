cbabf1edf4a32fd3d195a57f713b2b013869806a
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-02-27 19:37:56 +0400

    changing to GlobalMediaItem

--- KlipyiOS-demo/App/ViewUtilities/PreviewViewModel.swift
+++ KlipyiOS-demo/App/ViewUtilities/PreviewViewModel.swift
@@ -8,11 +8,10 @@
 import SwiftUI
 
 class PreviewViewModel: ObservableObject {
-  @Published var selectedItem: (item: GridItemLayout, frame: CGRect)? = nil
+  @Published var selectedItem: GlobalMediaItem?
   @Published var isDragging = false
   @Published var dragOffset: CGSize = .zero
   @Published var dragScale: CGFloat = 1.0
   @Published var pressScale: CGFloat = 1.0
-  
   @Published var isShowingReportMenu = false
 }
