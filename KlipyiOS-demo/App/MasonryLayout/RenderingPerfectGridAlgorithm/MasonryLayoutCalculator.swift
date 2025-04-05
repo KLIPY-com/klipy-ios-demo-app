cb6af69d8811c5b62b8b4931ad7c58549c9fc9d8
waska14 <waska@chaduneli.com>
2025-04-05 23:41:53 +0400

    Add support of scaling advertisement and min item width

--- KlipyiOS-demo/App/MasonryLayout/RenderingPerfectGridAlgorithm/MasonryLayoutCalculator.swift
+++ KlipyiOS-demo/App/MasonryLayout/RenderingPerfectGridAlgorithm/MasonryLayoutCalculator.swift
@@ -53,7 +53,7 @@ class MasonryLayoutCalculator {
         }
     }
     
-    private func calculateOptimalRow(_ possibleItems: [GridItemLayout]) -> ([GridItemLayout], CGFloat) {
+    private func calculateOptimalRow(_ possibleItems: [GridItemLayout], _ itemMinWidth: Int, _ adMaxResizePercent: Int) -> ([GridItemLayout], CGFloat) {
         var minimumChange = CGFloat.greatestFiniteMagnitude
         var currentRow: [GridItemLayout] = []
         var optimalHeight: CGFloat = 0
@@ -66,7 +66,7 @@ class MasonryLayoutCalculator {
         if let adIndex = adIndex, adIndex > 1 {
             // Limit to 2 items if ad is beyond position 1
             let items = Array(possibleItems.prefix(2))
-            return calculateOptimalRow(items)
+            return calculateOptimalRow(items, itemMinWidth, adMaxResizePercent)
         } else if let adIndex = adIndex {
             // If ad exists, use its height as fixed constraint
             currentMinHeight = possibleItems[adIndex].height
@@ -112,6 +112,52 @@ class MasonryLayoutCalculator {
             currentRow[i].height = optimalHeight
         }
         
+        // Scale advertisement if it's necessary + possible depending on itemMinWidth and adMaxResizePercent
+        if let adIndex = adIndex, nonAdItems.count != currentRow.count {
+            let itemsBelowMin = nonAdItems.filter { $0.width < CGFloat(itemMinWidth) }
+
+            if !itemsBelowMin.isEmpty {
+                // Set items to itemMinWidth
+                for i in 0..<currentRow.count {
+                    if currentRow[i].type != "ad", currentRow[i].width < CGFloat(itemMinWidth) {
+                        currentRow[i].width = CGFloat(itemMinWidth)
+                    }
+                }
+
+                // Recalculate total row width
+                let newRowWidth = currentRow.reduce(0) { $0 + $1.width } + CGFloat(currentRow.count - 1) * gap
+
+                if newRowWidth > containerWidth {
+                    var adItem = currentRow[adIndex]
+                    let minAdWidth = adItem.width * (100 - CGFloat(adMaxResizePercent)) / 100
+                    var resizedAdWidth = adItem.width - (newRowWidth - containerWidth)
+
+                    if resizedAdWidth < minAdWidth {
+                        let adWidthDifference = minAdWidth - resizedAdWidth
+                        for i in 0..<currentRow.count {
+                            if currentRow[i].type != "ad", currentRow[i].width == CGFloat(itemMinWidth) {
+                                currentRow[i].width -= adWidthDifference / CGFloat(itemsBelowMin.count)
+                            }
+                        }
+                        resizedAdWidth = minAdWidth
+                    }
+
+                    let scaleFactor = resizedAdWidth / adItem.width
+                    adItem.height *= scaleFactor
+                    adItem.width = resizedAdWidth
+                    adItem.newWidth = resizedAdWidth
+                    currentRow[adIndex] = adItem
+
+                    for i in 0..<currentRow.count {
+                        if currentRow[i].type != "ad" {
+                            currentRow[i].height = adItem.height
+                        }
+                    }
+                    optimalHeight = adItem.height
+                }
+            }
+        }
+
         return (currentRow, optimalHeight)
     }
     
@@ -140,7 +186,7 @@ class MasonryLayoutCalculator {
                 )
             }
             
-            let (rowItems, rowHeight) = calculateOptimalRow(possibleLayoutItems)
+            let (rowItems, rowHeight) = calculateOptimalRow(possibleLayoutItems, withMeta.adMaxResizePercent, withMeta.itemMinWidth)
             rows.append(RowLayout(items: rowItems, height: rowHeight))
             nextItem += rowItems.count
         }
